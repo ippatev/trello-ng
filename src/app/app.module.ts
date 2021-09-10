28df5c1fb243cc6cd6c94b5c1ad7ac9db6531efe
ippatiev_ag <alexanderippatiev24@gmail.com>
2021-09-10 12:24:22 +0900

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskListItemComponent } from './task-list-item/task-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskListItemComponent
   ],
   imports: [
     BrowserModule
